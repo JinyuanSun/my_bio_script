16c34434a2fec1e46ae3c0c57d5c1f32484fa147
jysun <1650949260@qq.com>
2021-08-29 22:19:54 +0800

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   .idea/workspace.xml
    	new file:   basic/__pycache__/fasta.cpython-37.pyc
    	modified:   basic/fasta.py

--- basic/fasta.py
+++ basic/fasta.py
@@ -22,11 +22,41 @@ def output_single_fa(fasta_dict): #split a fastadict into fasta file of single s
             outfile.close()
     return filename
 
+def split_fasta(fastafilename):
+    output_single_fa(fasta2dic(fastafilename))
+
 def read_a3m():
 
     return []
 
-def trim_blast_hits(btb,hitseqs):
-    hitdict = fasta2dic(hitseqs)
-    with open(btb) as blastfile:
+def align_2_seq(raw_seq1,raw_seq2):
+
+    from Bio import pairwise2
+    import pickle
+    with open("BLOSUM62.pkl", "rb") as tf:
+        matrix = pickle.load(tf)
+        tf.close()
+
+    seq1 = raw_seq1
+    seq2 = raw_seq2
+
+    alignments = pairwise2.align.globalds(seq1, seq2, matrix, -10, -0.5)
+    seq1 = alignments[0][0]
+    seq2 = alignments[0][1]
+    resnum = 0
+    #index = 0
+    aligned_seq2 = ''
+    for index in range(len(seq1)):
+        if seq1[index] == "-":
+            continue
+        else:
+            aligned_seq2 += seq2[index]
+            resnum += 1
+            if seq1[index] == seq2[index]:
+                continue
+            #else:
+                #print(seq1[index],resnum,seq2[index])
+        index += 1
+    #print(raw_seq1+"\n"+aligned_seq2)
+    return aligned_seq2
 
